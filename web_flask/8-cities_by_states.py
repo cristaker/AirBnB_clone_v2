b75a4a6edcb4ad8baf8d4f75f4f0afee5080d90e
cristaker <cristaker1927@hotmail.com>
2022-05-24 20:40:13 +0000

    point 9-10

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -3,6 +3,7 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from models.city import City
 
 
 app = Flask(__name__)
@@ -14,14 +15,13 @@ def teardown_session(exception):
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route('/cities_by_states', strict_slashes=False)
 def display_html():
     """ Function called with /states_list route """
     states = storage.all(State)
-    dict_to_html = {value.id: value.name for value in states.values()}
-    return render_template('7-states_list.html',
+    return render_template('8-cities_by_states.html',
                            Table="States",
-                           items=dict_to_html)
+                           states=states)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
